2002a0d10374fa06de424ef8be89632c0b916b76
Ronan1410 <1410ronan@gmail.com>
2026-07-30 17:18:21 +0530

    basic program counter

---
+++ Single_Cycle.v
@@ -0,0 +1,14 @@
+//Program Counter
+module Program_Counter(clk, rst, PC_in, PC_out);
+    input clk, rst;
+    input [31:0] PC_in;
+    output reg [31:0] PC_out;
+
+    always @(posedge clk or posedge rst)
+    begin
+        if(rst)
+            PC_out <= 32'b0;
+        else
+            PC_out <= PC_in;
+    end
+endmodule
